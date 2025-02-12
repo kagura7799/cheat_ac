a363eca01e7e31e29d312457c027d10f437c85d4
kagura7799 <mkotelnikov97@gmail.com>
2025-02-12 17:48:18 +0300

    cheat class created

---
+++ src/cheat.cpp
@@ -0,0 +1,53 @@
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <string>
+
+#include "offsets.cpp"
+
+class Cheat {
+private:
+    uintptr_t getModuleBaseAddress(DWORD pid, const std::wstring& moduleName) {
+        HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, pid);
+
+        if (!hSnapshot) {
+            std::cerr << "[ERROR] Failed to create modules snapshot (errno: " << GetLastError() << ")\n";
+            ExitProcess(1);
+        }
+
+        MODULEENTRY32W moduleEntry{};
+        moduleEntry.dwSize = sizeof(moduleEntry);
+
+        if (Module32FirstW(hSnapshot, &moduleEntry)) {
+            do {
+            if (moduleName == moduleEntry.szModule) {
+                CloseHandle(hSnapshot);
+                return reinterpret_cast<uintptr_t>(moduleEntry.modBaseAddr);
+            }
+            } while (Module32NextW(hSnapshot, &moduleEntry));
+        }
+
+        return NULL;
+    }
+
+public:
+    Cheat() {
+        DWORD pid;
+        HWND hwnd = FindWindowA(NULL, "AssaultCube"); 
+        GetWindowThreadProcessId(hwnd, &pid);
+
+        HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+
+        if (hProcess != NULL) {
+            std::cout << "Process opened" << std::endl;
+        }
+        
+        uintptr_t baseAddress = getModuleBaseAddress(pid, L"ac_client.exe");
+        
+        uintptr_t playerBase;
+        ReadProcessMemory(hProcess, (LPCVOID)(baseAddress + 0x195404), &playerBase, sizeof(uintptr_t), NULL);
+        std::cout << std::hex << playerBase << std::endl;
+    }
+
+    // TODO: func which add to endless hp, armor, ammo
+};
